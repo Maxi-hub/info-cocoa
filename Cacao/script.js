d5b073c9332ff53f0fb05171d65212ce5db85264
Emma <em.lebedeva85@gmail.com>
2024-10-08 11:19:36 +0300

    Changed maya calendar, added skip button for history page

--- Cacao/script.js
+++ Cacao/script.js
@@ -1,43 +1,65 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        var img = document.querySelector(".wallpaper__img");
-        img.classList.add("zoom-in"); // Добавляем класс для запуска анимации
-        document.querySelector('.header').style.display = 'none';
+const textBlock = document.querySelectorAll('.about__box');
+const buttonEl = document.querySelector('.button-skip');
+const headerEL = document.querySelector('.header');
+const wrapperBlock = document.querySelector('.wrapper');
+const wallpaperBlock = document.querySelector('.wallpaper');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const zoomWallpaper = setTimeout(() => {
+        const img = document.querySelector(".wallpaper__img");
+        img.classList.add("zoom-in"); 
+        headerEL.style.display = 'none';
     }, 7000);
 
-    setTimeout(function () {
-        document.querySelector('.wallpaper').style.display = 'none';
-        document.querySelector('.wrapper').style.display = 'block';
+    const appearPage = setTimeout(() => {
+        wallpaperBlock.style.display = 'none';
+        wrapperBlock.style.display = 'inline-block';
+
+        window.scrollTo({
+            top: 10,
+            left: 0,
+            behavior: "smooth",
+        });
+
     }, 10000); // 10000 миллисекунд = 10 секунд
 
-    setTimeout(function () {
-        document.querySelector('.header').style.display = 'flex';
-        document.querySelector('.header').style.animation = 'show 5s';
+    const appearHeader = setTimeout(() => {
+        headerEL.style.display = 'flex';
+        headerEL.style.animation = 'show 5s';
+    }, 13000);
+
+    buttonEl.addEventListener('click', function (e) {
+        wallpaperBlock.style.display = 'none';
+        wrapperBlock.style.animation = 'show 3s';
+        wrapperBlock.style.display = 'inline-block';
+        headerEL.style.display = 'flex';
+        headerEL.style.animation = 'show 4s';
 
         window.scrollTo({
             top: 220,
             left: 0,
             behavior: "smooth",
         });
-    }, 13000);
+
+        clearTimeout(zoomWallpaper);
+        clearTimeout(appearPage);
+        clearTimeout(appearHeader);
+    });
 });
-
-
-const textBlock = document.querySelectorAll('.about__box');
 
 textBlock.forEach(text => {
-    text.addEventListener('click', function (event) {
+    text.addEventListener('click', () => {
         text.classList.toggle('visible');
-        text.classList.toggle('transparent');
+        text.style.cursor = 'default';
     });
-    
-    document.addEventListener('click', function (event) {
-        if (!text.contains(event.target)) {
-            text.classList.remove('visible');
-            text.classList.add('transparent');
-        }
+
+    text.addEventListener('mouseenter', () => {
+        text.style.cursor = 'url(img/cursor-touch.png), auto';
     });
-    
 });
 
 
+
+// array.forEach(element => {
+    
+// });
